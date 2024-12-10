0f26544b85894f5a0b2bd38e32108cee65ff07b3
killeramoeba <killeramoeba@gmail.com>
2024-12-10 12:01:53 -0500

    removed empty interface declaration

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -4,9 +4,7 @@ import NavigationItem from "./NavigationItem";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-interface HeadingProps {}
-
-const Navigation: React.FC<HeadingProps> = ({}) => {
+const Navigation: React.FC = ({}) => {
   return (
     <div className="container mx-auto max-w-4xl flex flex-row p-1 items-center justify-end gap-4 z-10 px-4">
       <NavigationItem icon={faUser} text="About" url="/about" />
