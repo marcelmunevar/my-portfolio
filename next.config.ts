8e5ab79aa50ee9015bc8f7079150ce18917fca18
Marcel Munevar <marcelmunevar@gmail.com>
2024-12-13 06:51:50 -0500

    moved about to root

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/about',
-        permanent: true, // or false if you want a temporary redirect
-      },
-    ]
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -9,7 +9,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 const Navigation: React.FC = ({}) => {
   return (
     <div className="container mx-auto max-w-4xl flex flex-row p-1 items-center justify-end gap-4 z-10 px-4">
-      <NavigationItem icon={faUser} text="About" url="/about" />
+      <NavigationItem icon={faUser} text="About" url="/" />
       <NavigationItem icon={faFileLines} text="Resume" url="/resume" />
       <NavigationItem icon={faFile} text="Pages" url="/pages" />
       <NavigationItem icon={faGithub} text="Repos" url="/repos" />
