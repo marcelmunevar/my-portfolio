ec2d8b25a8c99bc2e50096f5e97b743cd8b1a3b3
Marcel Munevar <marcelmunevar@gmail.com>
2024-12-13 07:16:46 -0500

    optimized fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import Navigation from "./components/Navigation";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Marcel's React Portfolio",
@@ -14,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body className="">
         <GoogleAnalytics gaId="G-EV253K0KMF" />
         <Navigation />
