02834626ef59724fb8e1ff4f50045187069b2f2d
Marcel Munevar <marcelmunevar@gmail.com>
2025-01-07 15:36:15 -0500

    removed bold

--- src/app/components/Heading-3.tsx
+++ src/app/components/Heading-3.tsx
@@ -13,7 +13,7 @@ const Heading3: React.FC<HeadingProps> = ({ text, icon }) => {
       {icon && (
         <FontAwesomeIcon icon={icon} className="text-xl text-cyan-400 mr-4" />
       )}
-      <h2 className="text-xl lg:text-2xl font-bold leading-none tracking-tight mr-16 mb-4 lg:mb-0">
+      <h2 className="text-xl lg:text-2xl leading-none tracking-tight mr-16 mb-4 lg:mb-0">
         {text}
       </h2>
     </div>
