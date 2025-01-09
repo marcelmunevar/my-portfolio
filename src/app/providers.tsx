2482daad6fcec58caa8172702656c73a61ac9d6e
Marcel Munevar <marcelmunevar@gmail.com>
2025-01-09 15:54:16 -0500

    nextui routing

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRouter } from "next/navigation";
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={router.push}>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         {children}
       </NextThemesProvider>
