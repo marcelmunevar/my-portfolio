39daef66d02b67ebe4b25da1a1c858d645a83879
Marcel Munevar <marcelmunevar@gmail.com>
2025-04-29 21:45:30 -0400

    generateStaticParams

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,18 @@
 import { Suspense } from "react";
 import Post from "@/components/blog/Post";
 import BlogPostSkeleton from "@/components/blog/PostSkeleton";
+import { getPosts } from "@/utils/getPosts";
 
 interface PageProps {
   params: Promise<{ slug: string }>;
+}
+
+export async function generateStaticParams() {
+  const posts = await getPosts();
+
+  return posts.items.map((post) => ({
+    slug: post.fields.slug,
+  }));
 }
 
 export default async function BlogPage({ params }: PageProps) {
