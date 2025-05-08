aa735a07579966a5627e487ddafee701c07dd4f8
Marcel Munevar <marcelmunevar@gmail.com>
2025-05-07 23:19:21 -0400

    removed blurb from blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -14,14 +14,6 @@ export default function BlogHome() {
   return (
     <div className="container mx-auto px-8 max-w-2xl md:max-w-4xl mb-8">
       <Heading1 text="Blog" />
-      <p className="mb-4">
-        I&apos;ve integrated a streaming feature on my blog page to provide a
-        smoother and more dynamic reading experience. You might notice a brief
-        loading animation (skeletons) while the content is being fetched, but
-        rest assured, this is just a visual cue to enhance your browsing
-        experience. Enjoy seamless reading!{" "}
-      </p>
-
       <Suspense fallback={<BlogCardSkeleton />}>
         <Posts />
       </Suspense>
