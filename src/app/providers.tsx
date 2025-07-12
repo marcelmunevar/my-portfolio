809398ed8b37f926edd1ae1623cd1ab3602eafd9
Marcel Munevar <marcelmunevar@gmail.com>
2025-07-12 18:41:23 -0400

    fixed provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { HeroUIProvider } from "@heroui/system";
+import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useRouter } from "next/navigation";
 
