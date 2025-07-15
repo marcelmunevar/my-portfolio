8c6a1ca01d707ad1d29af4d52729547e38a766d7
Marcel Munevar <marcelmunevar@gmail.com>
2025-07-15 17:30:07 -0400

    providers fixed

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,14 @@ import { HeroUIProvider } from "@heroui/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useRouter } from "next/navigation";
 
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
