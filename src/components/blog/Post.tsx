eddf41ad8bf39835bd5903c8909ef78eab2a83de
Marcel Munevar <marcelmunevar@gmail.com>
2025-07-16 12:19:10 -0400

    added bottom margin to code snippets

--- src/components/blog/Post.tsx
+++ src/components/blog/Post.tsx
@@ -53,9 +53,11 @@ export default function Post({ post, image }: PostProps) {
           </h3>
         ),
         [BLOCKS.QUOTE]: (node: Block | Inline, children: React.ReactNode) => (
-          <Snippet symbol="">
-            <div className="whitespace-pre-wrap mt-4">{children}</div>
-          </Snippet>
+          <div className="mb-4">
+            <Snippet symbol="">
+              <div className="whitespace-pre-wrap mt-4">{children}</div>
+            </Snippet>
+          </div>
         ),
       },
       renderMark: {
