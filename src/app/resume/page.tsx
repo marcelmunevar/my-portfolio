be31026de3059a04fb76494913a161135df829e7
Marcel Munevar <marcelmunevar@gmail.com>
2025-11-22 09:58:02 -0500

    added react summit 2025

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -86,6 +86,11 @@ export default function Home() {
               <div className="mb-8">
                 <Heading3 icon={faUsers} text="Conferences" />
                 <ResumeItem
+                  date="November 2025"
+                  institution="React Summit"
+                  title="NYC (virtual)"
+                />
+                <ResumeItem
                   date="September 2025"
                   institution="Techlead Conf: AI In Orgs"
                   title="Remote"
