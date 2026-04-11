e320617194b7e561c384186d363b76b2e0abf079
marcelmunevar <marcelmunevar@gmail.com>
2026-04-11 19:11:12 -0400

    Remove Clients section from the homepage
    Removed the Clients section and associated Card component from the main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Heading1 from "../components/common/Heading-1";
 import Heading2 from "../components/common/Heading-2";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { Card, CardBody } from "@heroui/card";
 import ClientBreadcrumbs from "@/components/common/ClientBreadcrumbs";
 import CardSection from "@/components/about/CardSection";
 import { getHomepagePosts } from "@/utils/getHomepagePosts";
@@ -20,19 +19,7 @@ export default async function Home() {
         </div>
 
         <CardSection homepagePostsResponse={homepagePostsResponse} />
-        <div className="container mx-auto px-8 max-w-2xl md:max-w-4xl lg:max-w-4xl mb-12">
-          <Heading2 text="Clients" />
-          <Card className="fade-in-right">
-            <CardBody>
-              <p>
-                I currently support Fujifilm Holdings America Corporation, which
-                oversees more than two dozen businesses across the Americas. My
-                work spans a diverse range of industries, including healthcare,
-                manufacturing, printing, and more.
-              </p>
-            </CardBody>
-          </Card>
-        </div>
+        
       </main>
     </div>
   );
