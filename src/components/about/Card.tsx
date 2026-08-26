707d11d54e1c86da26b8369cb7e398bee5dc8a64
Marcel Munevar <marcelmunevar@gmail.com>
2026-08-26 09:39:04 -0400

    update Card component to use a div instead of a paragraph for description rendering (fixed hydration error)

--- src/components/about/Card.tsx
+++ src/components/about/Card.tsx
@@ -71,7 +71,7 @@ const Card: React.FC<CardProps> = ({
           <div className="md:col-span-3">
             <Heading3 text={heading} disableFade={true} />
             {description ? (
-              <p dangerouslySetInnerHTML={{ __html: description }}></p>
+              <div dangerouslySetInnerHTML={{ __html: description }}></div>
             ) : (
               false
             )}
